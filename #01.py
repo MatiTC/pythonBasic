9aa872b81800ed884f7eebfb0386fa618e892d43
Matias Tardones <mtardones99@gmail.com>
2024-04-17 23:11:04 -0400

    probando python

---
+++ #01.py
@@ -0,0 +1,20 @@
+#Operadores 
+suma = 5 + 1
+print(suma)
+modulo = 10%3
+print(modulo)
+
+#Estructura de control
+x = 10
+y = 20
+#condicional
+if y > x:
+    print(f'Y:es mayor que:{y}')
+elif x == y:
+    print("No son iguales")
+else:
+    print(f"X es menos que {y}")
+
+#Interactivo 
+for i in range(10):
+    print(i)
---
+++ main.py
@@ -0,0 +1,17 @@
+print('Hola mundo')
+
+"""
+Esto es un comentario
+"""
+'''
+Esto también es un comentario
+'''
+
+my_variable = "Mi variable"
+print(my_variable)
+my_variable = "Soy un nuevo valor"
+print(my_variable)
+my_variable = 1
+print(my_variable)
+myVariable = "¡Hola, [y el nombre de tu lenguaje]!"
+print(myVariable)
